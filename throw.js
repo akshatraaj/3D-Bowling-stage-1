03f69d895fa905a6c9ce19c84c9e4853af4dc5fc
Akshat Raaj <rvdewsoft@gmail.com>
2021-05-24 20:08:18 +0530

    Add files via upload

---
+++ throw.js
@@ -0,0 +1,29 @@
+AFRAME.registerComponent("bowling-balls", {
+  init: function () {
+    this.throwBall();
+  },
+  throwBall: function () {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "z") {
+        var  ball = document.createElement("a-entity");
+        ball.setAttribute("gltf-model", "./models/bowling_ball/scene.gltf");
+        ball.setAttribute("scale", { x: 3, y: 3,  z: 3});
+        var cam = document.querySelector("#camera");
+        pos = cam.getAttribute("position");
+        ball.setAttribute("position", {
+          x: pos.x,
+          y: pos.y-1.2,
+          z: pos.z,
+        });
+        var camera = document.querySelector("#camera").object3D;
+        var direction = new THREE.Vector3();
+        camera.getWorldDirection(direction);
+        ball.setAttribute("velocity", direction.multiplyScalar(-10));
+        var scene = document.querySelector("#scene");
+        scene.appendChild(ball);
+      }
+    });
+  },
+});
+
+
